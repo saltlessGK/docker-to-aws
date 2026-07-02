fe37818bf68914c1fc3be79b156d651c8040e66d
saltlessGK <s4035894@rmit.edu.vn>
2026-07-02 18:24:07 +0700

    Reconfigured S3 bucket for terraform state storage

--- terraform-bootstrap/main.tf
+++ terraform-bootstrap/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "terraform-state-s4035894"
+  bucket = "terraform-state-saltlessGK"
 
   tags = {
     Name = "Terraform State Bucket"
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "terraform-state-s4035894"
+    bucket  = "terraform-state-saltlessGK"
     key     = "./terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
